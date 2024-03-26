bb5c2880adfab71d99d359a54c1df9d26d18f6a8
ProfCastello <gustavo.castello@sp.senai.br>
2024-03-26 10:16:41 -0300

    Alterações do dia 26-03

--- Aula02/js/script.js
+++ Aula02/js/script.js
@@ -1,3 +1,17 @@
 alerta = () => {
   alert("Olá, eu sou um alerta!🚨");
 };
+
+const enviar = () => {
+  let nome = document.getElementById("nome").value;
+
+  document.getElementById(
+    "resposta"
+  ).innerHTML = `<h2>Olá ${nome}, formulário enviado com sucesso!</h2>`;
+
+  if (nome == "") {
+    alert("Bem vindo!");
+  } else {
+    alert(`Olá ${nome}, bem vindo!`);
+  }
+};
